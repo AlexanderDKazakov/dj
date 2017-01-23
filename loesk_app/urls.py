c43867d7d7ff54c0f6b94b43e0183d9bde0dc739
Saratov <Saratov@ex.com>
2017-01-23 14:52:58 +0300

    Add adresses to loesk_app/url.py

--- loesk_app/urls.py
+++ loesk_app/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^basicview/', include('call.urls')),
+    url(r'^auth/', include('loginsys.urls')),
+    url(r'^', include('call.urls')),
+
 ]
