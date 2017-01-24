ed133b5a7a786d01d6f40144bce78ac2676fbcb0
Saratov <Saratov@ex.com>
2017-01-24 12:28:31 +0300

    Moving complete

---
+++ loginsys/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render_to_response, redirect
+from django.contrib import auth
+from django.core.context_processors import csrf
+
+# Create your views here.
+
+
+def login(request):
+    args = {}
+    args.update(csrf(request))
+    if request.POST:
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            args['login_error'] = "Пользователь не найден"
+            return render_to_response('login.html', args)
+
+    else:
+        return render_to_response('login.html', args)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
