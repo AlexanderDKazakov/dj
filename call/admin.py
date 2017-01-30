7ac2038447dcb58e5bbed54cda31dd88b313726c
Saratov <Saratov@ex.com>
2017-01-30 19:10:04 +0300

    30.01.17 popup windows --- creating exporting data from admin panel. Done. --- CSV --- XLS --- XLSX

--- call/admin.py
+++ call/admin.py
@@ -2,8 +2,118 @@ from django.contrib import admin
 from call.models import Call, Profile, Filial, legalEntity, Aim_call, Res, Otdel, ActOperator
 from attachments.admin import AttachmentInlines
 
-class CallAdmin(admin.ModelAdmin):
+######## EXPORTING
+from django.http import HttpResponse
 
+def export_csv(modeladmin, request, queryset):
+    import csv
+    from django.utils.encoding import smart_str
+    # response = HttpResponse(mimetype='text/csv')
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=mymodel.csv'
+    writer = csv.writer(response, csv.excel)
+    response.write(u'\ufeff'.encode('utf8')) # BOM (optional...Excel needs it to open UTF-8 file properly)
+    writer.writerow([
+        smart_str(u"ID"),
+        smart_str(u"call_title"),
+        smart_str(u"call_aim"),
+    ])
+    for obj in queryset:
+        writer.writerow([
+            smart_str(obj.pk),
+            smart_str(obj.call_title),
+            smart_str(obj.call_aim),
+        ])
+    return response
+export_csv.short_description = u"Export CSV"
+
+
+def export_xls(modeladmin, request, queryset):
+    import xlwt
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename=mymodel.xls'
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet("MyModel")
+
+    row_num = 0
+
+    columns = [
+        (u"ID", 2000),
+        (u"ФИО Абонента", 6000),
+        (u"Причина звонка", 8000),
+    ]
+
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num][0], font_style)
+        # set column width
+        ws.col(col_num).width = columns[col_num][1]
+
+    font_style = xlwt.XFStyle()
+    font_style.alignment.wrap = 1
+
+    for obj in queryset:
+        row_num += 1
+        row = [
+            obj.pk,
+            obj.call_title,
+            str(obj.call_aim),
+        ]
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, row[col_num], font_style)
+
+    wb.save(response)
+    return response
+
+
+export_xls.short_description = u"Export XLS"
+
+def export_xlsx(modeladmin, request, queryset):
+    import openpyxl
+    from openpyxl.utils import get_column_letter
+    # from openpyxl.cell import get_column_letter
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename=mymodel.xlsx'
+    wb = openpyxl.Workbook()
+    ws = wb.get_active_sheet()
+    ws.title = "MyModel"
+
+    row_num = 0
+
+    columns = [
+        (u"ID", 15),
+        (u"call_title", 70),
+        (u"call_aim", 70),
+    ]
+
+    for col_num in range(len(columns)):
+        c = ws.cell(row=row_num + 1, column=col_num + 1)
+        c.value = columns[col_num][0]
+        # c.style.font.bold = True
+        # set column width
+        ws.column_dimensions[get_column_letter(col_num+1)].width = columns[col_num][1]
+
+    for obj in queryset:
+        row_num += 1
+        row = [
+            obj.pk,
+            obj.call_title,
+            str(obj.call_aim),
+        ]
+        for col_num in range(len(row)):
+            c = ws.cell(row=row_num + 1, column=col_num + 1)
+            c.value = row[col_num]
+            # c.style.alignment.wrap_text = True
+
+    wb.save(response)
+    return response
+
+export_xlsx.short_description = u"Export XLSX"
+#####
+class CallAdmin(admin.ModelAdmin):
+    actions = [export_csv, export_xls, export_xlsx]
     list_filter = ['call_entite',
                    'call_date',
                    'call_user_man',
--- call/static/scripts.js
+++ call/static/scripts.js
@@ -1,22 +1,34 @@
-        var $input = $("#id_call_otvet");
-                if ($input.prop('checked')){
-                    $("#id_call_kontact").css({'display': 'inline-block'});
-                }
-                else {
-                    $("#id_call_kontact").css({'display': 'none'});
-                }
-       $("#id_call_otvet").click(function(){
-           var $input = $(this);
-                if ($input.prop('checked')){
-                    $("#id_call_kontact").css({'display': 'inline-block'});
-                }
-                else {
-                    $("#id_call_kontact").css({'display': 'none'});
-                }
-       })
-
-        var options = {
-        valueNames: [ 'name', 'date', 'aim' ]
-        };
-
-        var userList = new List('users', options);
+//INITIAL CONDITIONS
+PopUpHide();
+//
+// HIDE/SHOW FIELD FOR CONTACT INFORMATION
+var $input = $("#id_call_otvet");
+    if ($input.prop('checked')){
+        $("#id_call_kontact").css({'display': 'inline-block'});
+    }
+    else {
+        $("#id_call_kontact").css({'display': 'none'});
+    }
+$("#id_call_otvet").click(function(){
+    var $input = $(this);
+    if ($input.prop('checked')){
+        $("#id_call_kontact").css({'display': 'inline-block'});
+    }
+    else {
+        $("#id_call_kontact").css({'display': 'none'});
+    }
+})
+// END H/S
+// SORTING&FILTERS
+var options = {
+    valueNames: [ 'name', 'date', 'aim' ]
+};
+var userList = new List('users', options);
+// END S&F
+// PUPOP WINDOW
+function PopUpShow(){
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+}
