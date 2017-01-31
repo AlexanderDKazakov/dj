a7d9d711a7061a9cba46e68e9c7671a8f71bbfe5
Saratov <Saratov@ex.com>
2017-01-31 22:41:42 +0300

    31.01.17 --- From AdminPanel --- alles gut. --- From UserInterface --- need more time.
    configure exporting data from admin panel. Need a tests.

--- call/urls.py
+++ call/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
     url(r'^call/get/(?P<call_id>[0-9]+)/$', 'call.views.call_edit'),
     url(r'^new$', 'call.views.new_call'),
-    url(r'^data_out_csv$', 'call.views.data_out_csv'),
+    url(r'^data_out$', 'call.views.export_xls'),
     # url(r'^data_out_pdf$', 'call.views.data_out_pdf'),
     url(r'^$', 'call.views.list_call'),
     url(r'^login', 'loginsys.views.login'),
